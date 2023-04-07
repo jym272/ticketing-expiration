98b26251092087452407919242f63a71722e277b
Jorge Clavijo <jym272@gmail.com>
2023-04-07 04:51:16 -0300

    create and verify consumers and streams complete

--- nats/nats.go
+++ nats/nats.go
@@ -1,130 +1,182 @@
 package main
 
 import (
-	"fmt"
 	"github.com/nats-io/nats.go"
+	"strings"
+
+	"fmt"
 	"os"
 )
 
 const (
-	Tickets string = "tickets"
-	Orders  string = "orders"
+	Tickets        string = "tickets"
+	Orders         string = "orders"
+	OrderCreated   string = "orders.created"
+	OrderCancelled string = "orders.cancelled"
+	TicketCreated  string = "tickets.created"
+	TicketUpdated  string = "tickets.updated"
 )
 
-var nc *nats.Conn
+type Stream struct {
+	name     string
+	subjects []string
+}
 
-func createStream() {
+type Context struct {
+	nc      *nats.Conn
+	js      nats.JetStreamContext
+	streams []Stream
+}
+
+func validateStream(stream string, subjects []string) {
+	if stream == "" {
+		panic("stream name cannot be empty")
+	}
+	if len(subjects) == 0 {
+		panic("subjects cannot be empty in stream: " + stream)
+	}
+	for _, subject := range subjects {
+		if subject == "" {
+			panic("subject cannot be empty in stream: " + stream)
+		}
+		if !strings.HasPrefix(subject, stream+".") {
+			panic("subject: " + subject + " does not start with stream: " + stream + ".")
+		}
+	}
+}
+
+func (s Stream) CreateStream(stream string, subjects []string) Stream {
+	validateStream(stream, subjects)
+	s.name = stream
+	s.subjects = subjects
+	return s
+}
+
+func (c *Context) AddStreams() {
+	ticketStream := Stream{}.CreateStream(Tickets, []string{TicketCreated, TicketUpdated})
+	ordersStream := Stream{}.CreateStream(Orders, []string{OrderCreated, OrderCancelled})
+	c.streams = []Stream{ticketStream, ordersStream}
+}
+
+func (c *Context) connectToNats() {
 	url := os.Getenv("NATS_URL")
 	if url == "" {
 		url = nats.DefaultURL
 	}
-	nc, _ = nats.Connect(url)
-	defer func(nc *nats.Conn) {
-		err := nc.Drain()
-		if err != nil {
-			fmt.Println("Error draining.", err)
-		}
-	}(nc)
 
-	js, _ := nc.JetStream()
+	nc, err := nats.Connect(url)
+	if err != nil {
+		fmt.Println("Error connecting to NATS.", err)
+		panic(err)
+	}
 
-	streamName := Orders
-	subj := streamName + ".>"
+	c.nc = nc
+	c.js, err = nc.JetStream()
+	if err != nil {
+		fmt.Println("Error creating JetStream context.", err)
+		panic(err)
+	}
+}
+
+func createStream(js nats.JetStreamContext, stream string) {
+	subj := stream + ".>" // ie: tickets.> -> tickets.one, tickets.one.two
 
 	_, err := js.AddStream(&nats.StreamConfig{
-		Name:     streamName,
+		Name:     stream,
 		Subjects: []string{subj}, // any number of tokens->ie: events.one, events.one.two
 	})
 	if err != nil {
 		fmt.Println("Error creating stream.", err)
 		panic(err)
-		return
 	}
-	fmt.Println("Stream created:", streamName)
 
 }
+
+func findStream(js nats.JetStreamContext, stream string) bool {
+	for name := range js.StreamNames() {
+		if name == stream {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Context) verifyStreams() {
+	for _, stream := range c.streams {
+		name := stream.name
+		if !findStream(c.js, name) {
+			fmt.Println("Stream not found:", name, "Creating...")
+			createStream(c.js, name)
+			fmt.Println("Stream created:", name)
+		} else {
+			fmt.Println("Stream found:", name)
+		}
+	}
+}
+
+func findConsumer(js nats.JetStreamContext, stream string, durableName string) bool {
+	for consumerInfo := range js.Consumers(stream) {
+		if consumerInfo.Config.Durable == durableName {
+			return true
+		}
+	}
+	return false
+}
+
+func createConsumer(js nats.JetStreamContext, stream string, durableName string, queueGroupName string, filterSubject string) {
+	_, err := js.AddConsumer(stream, &nats.ConsumerConfig{
+		Durable:        durableName,
+		DeliverPolicy:  nats.DeliverAllPolicy,
+		AckPolicy:      nats.AckExplicitPolicy,
+		DeliverSubject: nats.NewInbox(),
+		DeliverGroup:   queueGroupName,
+		FilterSubject:  filterSubject,
+	})
+	if err != nil {
+		fmt.Println("Error creating consumer.", err)
+		panic(err)
+	}
+}
+
+func createConsumerProps(subject string) (string, string, string) {
+	parts := strings.Split(subject, ".")
+	if len(parts) == 0 {
+		panic("Subject is empty")
+	}
+	var upperCaseParts []string
+	for _, part := range parts {
+		upperCaseParts = append(upperCaseParts, strings.ToUpper(part))
+	}
+	durableName := strings.Join(upperCaseParts, "_")
+	return durableName, subject, subject
+}
+
+func (c *Context) verifyConsumers() {
+	for _, stream := range c.streams {
+		for _, subject := range stream.subjects {
+			durableName, queueGroupName, filterSubject := createConsumerProps(subject)
+			if !findConsumer(c.js, stream.name, durableName) {
+				fmt.Println("Consumer not found:", durableName, "Creating consumer...")
+				createConsumer(c.js, stream.name, durableName, queueGroupName, filterSubject)
+				fmt.Println("Consumer created:", durableName)
+			} else {
+				fmt.Println("Consumer found:", durableName)
+			}
+		}
+	}
+}
+
 func main() {
-	// first find stream, if is not yet created, create it
-	createStream()
-}
+	ctx := Context{}
+	ctx.AddStreams()
+	ctx.connectToNats()
+	ctx.verifyStreams()
+	ctx.verifyConsumers()
 
-//
-//func main() {
-//
-//	fmt.Println("# Durable (implicit)")
-//	sub1, _ := js.QueueSubscribeSync(subj, "event-processor", nats.AckExplicit())
-//
-//	info, _ := js.ConsumerInfo(streamName, "event-processor")
-//	fmt.Printf("deliver group: %q\n", info.Config.DeliverGroup)
-//
-//	sub2, _ := js.QueueSubscribeSync("events.>", "event-processor", nats.AckExplicit())
-//
-//	sub3, _ := nc.QueueSubscribeSync(info.Config.DeliverSubject, info.Config.DeliverGroup)
-//	fmt.Printf("deliver subject: %q\n", info.Config.DeliverSubject)
-//
-//	js.Publish("events.1", nil)
-//	js.Publish("events.2", nil)
-//	js.Publish("events.3", nil)
-//	js.Publish("events.4", nil)
-//	js.Publish("events.5", nil)
-//	js.Publish("events.6", nil)
-//
-//	msg, _ := sub1.NextMsg(time.Second)
-//	if msg != nil {
-//		fmt.Printf("sub1: received message %q\n", msg.Subject)
-//		msg.Ack()
-//	} else {
-//		fmt.Println("sub1: receive timeout")
-//	}
-//
-//	msg, _ = sub2.NextMsg(time.Second)
-//	if msg != nil {
-//		fmt.Printf("sub2: received message %q\n", msg.Subject)
-//		msg.Ack()
-//	} else {
-//		fmt.Println("sub2: receive timeout")
-//	}
-//
-//	msg, _ = sub3.NextMsg(time.Second)
-//	if msg != nil {
-//		fmt.Printf("sub3: received message %q\n", msg.Subject)
-//		msg.Ack()
-//	} else {
-//		fmt.Println("sub3: receive timeout")
-//	}
-//
-//	sub1.Unsubscribe()
-//	sub2.Unsubscribe()
-//	sub3.Unsubscribe()
-//
-//	fmt.Println("\n# Durable (explicit)")
-//
-//	js.AddConsumer(streamName, &nats.ConsumerConfig{
-//		Durable:        "event-processor",
-//		DeliverSubject: "my-subject",
-//		DeliverGroup:   "event-processor",
-//		AckPolicy:      nats.AckExplicitPolicy,
-//	})
-//	defer js.DeleteConsumer(streamName, "event-processor")
-//
-//	wg := &sync.WaitGroup{}
-//	wg.Add(6)
-//
-//	sub1, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
-//		fmt.Printf("sub1: received message %q\n", msg.Subject)
-//		msg.Ack()
-//		wg.Done()
-//	})
-//	sub2, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
-//		fmt.Printf("sub2: received message %q\n", msg.Subject)
-//		msg.Ack()
-//		wg.Done()
-//	})
-//	sub3, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
-//		fmt.Printf("sub3: received message %q\n", msg.Subject)
-//		msg.Ack()
-//		wg.Done()
-//	})
-//
-//	wg.Wait()
-//}
+	defer func(nc *nats.Conn) {
+		err := nc.Drain()
+		if err != nil {
+			fmt.Println("Error draining.", err)
+		}
+	}(ctx.nc)
+}
---
+++ nats/natsfsdf.go
@@ -0,0 +1,81 @@
+
+//
+//func main() {
+//
+//	fmt.Println("# Durable (implicit)")
+//	sub1, _ := js.QueueSubscribeSync(subj, "event-processor", nats.AckExplicit())
+//
+//	info, _ := js.ConsumerInfo(streamName, "event-processor")
+//	fmt.Printf("deliver group: %q\n", info.Config.DeliverGroup)
+//
+//	sub2, _ := js.QueueSubscribeSync("events.>", "event-processor", nats.AckExplicit())
+//
+//	sub3, _ := nc.QueueSubscribeSync(info.Config.DeliverSubject, info.Config.DeliverGroup)
+//	fmt.Printf("deliver subject: %q\n", info.Config.DeliverSubject)
+//
+//	js.Publish("events.1", nil)
+//	js.Publish("events.2", nil)
+//	js.Publish("events.3", nil)
+//	js.Publish("events.4", nil)
+//	js.Publish("events.5", nil)
+//	js.Publish("events.6", nil)
+//
+//	msg, _ := sub1.NextMsg(time.Second)
+//	if msg != nil {
+//		fmt.Printf("sub1: received message %q\n", msg.Subject)
+//		msg.Ack()
+//	} else {
+//		fmt.Println("sub1: receive timeout")
+//	}
+//
+//	msg, _ = sub2.NextMsg(time.Second)
+//	if msg != nil {
+//		fmt.Printf("sub2: received message %q\n", msg.Subject)
+//		msg.Ack()
+//	} else {
+//		fmt.Println("sub2: receive timeout")
+//	}
+//
+//	msg, _ = sub3.NextMsg(time.Second)
+//	if msg != nil {
+//		fmt.Printf("sub3: received message %q\n", msg.Subject)
+//		msg.Ack()
+//	} else {
+//		fmt.Println("sub3: receive timeout")
+//	}
+//
+//	sub1.Unsubscribe()
+//	sub2.Unsubscribe()
+//	sub3.Unsubscribe()
+//
+//	fmt.Println("\n# Durable (explicit)")
+//
+//	js.AddConsumer(streamName, &nats.ConsumerConfig{
+//		Durable:        "event-processor",
+//		DeliverSubject: "my-subject",
+//		DeliverGroup:   "event-processor",
+//		AckPolicy:      nats.AckExplicitPolicy,
+//	})
+//	defer js.DeleteConsumer(streamName, "event-processor")
+//
+//	wg := &sync.WaitGroup{}
+//	wg.Add(6)
+//
+//	sub1, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
+//		fmt.Printf("sub1: received message %q\n", msg.Subject)
+//		msg.Ack()
+//		wg.Done()
+//	})
+//	sub2, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
+//		fmt.Printf("sub2: received message %q\n", msg.Subject)
+//		msg.Ack()
+//		wg.Done()
+//	})
+//	sub3, _ = nc.QueueSubscribe("my-subject", "event-processor", func(msg *nats.Msg) {
+//		fmt.Printf("sub3: received message %q\n", msg.Subject)
+//		msg.Ack()
+//		wg.Done()
+//	})
+//
+//	wg.Wait()
+//}
